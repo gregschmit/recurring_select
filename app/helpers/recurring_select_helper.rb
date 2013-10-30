bd374b33b2304dff829a418ec259cc1e6fd5cdd0
Forrest Zeisler <forrest@getjobber.com>
2013-10-30 11:25:59 -0600

    yet another attempt at better development [#22]

--- app/helpers/recurring_select_helper.rb
+++ app/helpers/recurring_select_helper.rb
@@ -4,12 +4,12 @@ require_relative "utilities/tag_ext"
 
 module RecurringSelectHelper
   module FormHelper
-    def select_recurring(object, method, default_schedules = nil, options = {}, html_options = {})
-      if Rails::VERSION::STRING.to_f >= 4.0
-        # === Rails 4
+    if Rails::VERSION::MAJOR == 4
+      def select_recurring(object, method, default_schedules = nil, options = {}, html_options = {})
         RecurringSelectTag.new(object, method, self, default_schedules, options, html_options).render
-      else
-        # === Rails 3
+      end
+    elsif Rails::VERSION::MAJOR == 3
+      def select_recurring(object, method, default_schedules = nil, options = {}, html_options = {})
         InstanceTag.new(object, method, self, options.delete(:object)).to_recurring_select_tag(default_schedules, options, html_options)
       end
     end
