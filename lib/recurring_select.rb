381b40c5e293fbcf204bc430edd9ae2d474ab07d
Damien Imberdis <imberdis.damien@gmail.com>
2014-02-09 22:14:34 -0500

    save the hour_of_day and minute_of_hour as validations + basic refactoring

--- lib/recurring_select.rb
+++ lib/recurring_select.rb
@@ -7,34 +7,19 @@ module RecurringSelect
     if params.is_a? IceCube::Rule
       params
     else
-      if params.is_a?(String)
-        params = JSON.parse(params)
-      end
-
-      params = params.symbolize_keys
-      rules_hash = filter_params(params)
-
+      params = JSON.parse(params) if params.is_a?(String)
+      rules_hash = filter_params(params.symbolize_keys!)
       IceCube::Rule.from_hash(rules_hash)
     end
   end
 
   def self.is_valid_rule?(possible_rule)
-    return true if possible_rule.is_a?(IceCube::Rule)
     return false if possible_rule.blank?
-
-    if possible_rule.is_a?(String)
-      begin
-        JSON.parse(possible_rule)
-        return true
-      rescue JSON::ParserError
-        return false
-      end
+    case possible_rule
+    when IceCube::Rule, Hash then true
+    when String then !!JSON.parse(possible_rule) rescue false
+    else false
     end
-
-    # TODO: this should really have an extra step where it tries to perform the final parsing
-    return true if possible_rule.is_a?(Hash)
-
-    false #only a hash or a string of a hash can be valid
   end
 
   private
@@ -48,32 +33,20 @@ module RecurringSelect
     params[:validations] ||= {}
     params[:validations].symbolize_keys!
 
-    if params[:validations][:day]
-      params[:validations][:day] = params[:validations][:day].collect(&:to_i)
-    end
-
-    if params[:validations][:day_of_month]
-      params[:validations][:day_of_month] = params[:validations][:day_of_month].collect(&:to_i)
+    [:day, :day_of_month, :day_of_year, :hour_of_day, :minute_of_hour].each do |key|
+      val = params[:validations][key]
+      params[:validations][key] = (val.is_a? Array) ? val.collect(&:to_i) : val.to_i if val
     end
 
     if params[:validations][:day_of_week]
-      params[:validations][:day_of_week] ||= {}
       if params[:validations][:day_of_week].length > 0 and not params[:validations][:day_of_week].keys.first =~ /\d/
         params[:validations][:day_of_week].symbolize_keys!
       else
         originals = params[:validations][:day_of_week].dup
         params[:validations][:day_of_week] = {}
-        originals.each{|key, value|
-          params[:validations][:day_of_week][key.to_i] = value
-        }
+        originals.each { |key, value| params[:validations][:day_of_week][key.to_i] = value }
       end
-      params[:validations][:day_of_week].each{|key, value|
-        params[:validations][:day_of_week][key] = value.collect(&:to_i)
-      }
-    end
-
-    if params[:validations][:day_of_year]
-      params[:validations][:day_of_year] = params[:validations][:day_of_year].collect(&:to_i)
+      params[:validations][:day_of_week].each{ |key, value| params[:validations][:day_of_week][key] = value.collect(&:to_i) }
     end
 
     params
